45e08a23e487d856d29c8eea934be4d8fa820b12
Lauras Dilys <lauras.dilys@gmail.com>
2021-10-01 02:30:25 +0300

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import './App.css';
 import Combat from './components/Combat';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import Navbar from './nav/Navbar';
 
 const App = () => {
   return (
-    <Combat />
+    <Router>
+      {/* <Navbar /> */}
+      <Switch>
+        {/* <Route path='/' exact component={Home} /> */}
+        <Route path='/' component={Combat} />
+      </Switch>
+    </Router>
   );
 }
 
