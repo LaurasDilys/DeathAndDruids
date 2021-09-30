5c17822cae409058f0bdd5fa3668f11b0bd8da96
Lauras Dilys <lauras.dilys@gmail.com>
2021-09-30 12:36:06 +0300

    sorted() now doesn't mutate state, handleCloseTab selects next tab

--- frontend/src/components/Combat.js
+++ frontend/src/components/Combat.js
@@ -8,65 +8,71 @@ import { getColor, FadingTab } from './CombatTabs/FadingTab';
 
 const mockCharacters = [{
   id: 1,
-  name: "Kresentas",
+  name: "Sixth",
   initiative: 1,
   hp: 1,
   maxHp: 11
 }, {
   id: 2,
-  name: "Negua",
+  name: "Fifth",
   initiative: 2,
   hp: 1,
   maxHp: 10
 }, {
   id: 3,
-  name: "Uranas",
+  name: "Fourth",
   initiative: 4,
   hp: 2,
   maxHp: 10
 }, {
   id: 4,
-  name: "Kresentas",
+  name: "Third",
   initiative: 6,
   hp: 4,
   maxHp: 10
 }, {
   id: 5,
-  name: "Negua",
+  name: "Second",
   initiative: 8,
   hp: 6,
   maxHp: 10
 }, {
   id: 6,
-  name: "Uranas",
+  name: "First",
   initiative: 10,
   hp: 8,
   maxHp: 10
 }]
 
 const sorted = (characters) => {
-  return characters.sort((a, b) => {
+  return [...characters].sort((a, b) => {
     return b.initiative - a.initiative;
   });
 }
 
+const firstInInitiative = (characters) => {
+  const list = sorted(characters);
+  return list.length === 0 ? 0 : list[0].id;
+}
+
 const CombatTabs = () => {
   const [characters, setCharacters] = useState(mockCharacters);
-  const [selectedTab, setSelectedTab] = useState(sorted(characters)[0].id);
-  
+  const [selectedTab, setSelectedTab] = useState(firstInInitiative(characters));
+
   const handleSelectedTabChange = (event, newValue) => {
     setSelectedTab(newValue);
   };
 
   const handleInitiativeChange = (id, initiative) => {
     setCharacters(characters.map(c => {
-      console.log();
       if (c.id === id) c.initiative = initiative;
       return c;
     }));
   };
 
   const handleCloseTab = (id) => {
+    const ids = sorted(characters).map(c => c.id);
+    setSelectedTab(ids[ids.indexOf(id) + 1]); // set to next tab in tabs row
     setCharacters(characters.filter(c => c.id !== id));
   }
 
@@ -74,7 +80,7 @@ const CombatTabs = () => {
     <Box sx={{ width: '100%', typography: 'body1' }}>
       <TabContext value={selectedTab}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleSelectedTabChange} aria-label="lab API tabs example">
+          <TabList variant="scrollable" scrollButtons="auto" onChange={handleSelectedTabChange}>
             {sorted(characters).map(character =>
               <FadingTab
                 style={getColor(character.hp / character.maxHp)}
