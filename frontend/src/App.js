701877645e2b525cb1d988f03472826fe478a0a8
Lauras Dilys <lauras.dilys@gmail.com>
2021-10-09 17:56:43 +0300

    prepared for location tracking (needed for combat)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
 import Routes from './Routes';
 import SideNav from './components/SideNav/SideNav';
+
+const LocationTracker = (props) => {
+  const location = useLocation();
+  console.log(location);
+
+  return props.children;
+}
 
 const App = () => {
   return (
     <Router>
-      <SideNav routes={Routes} />
-      <Switch>
-        {Routes.map(({ path, getComponent }, index) => <Route
-          key={index}
-          exact={path === "/"}
-          path={path}
-          component={getComponent()} />)}
-      </Switch>
+      <LocationTracker>
+        <SideNav routes={Routes} />
+        <Switch>
+          {Routes.map(({ path, getComponent }, index) => <Route
+            key={index}
+            exact={path === "/"}
+            path={path}
+            component={getComponent()} />)}
+        </Switch>
+      </LocationTracker>
     </Router>
   );
 }
