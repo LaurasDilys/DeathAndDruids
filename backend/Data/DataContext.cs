1341963a39fdf374c76b0fee43e230ae957a974a
Lauras Dilys <lauras.dilys@gmail.com>
2021-10-09 20:32:29 +0300

    Added Location and Combat

--- backend/Data/DataContext.cs
+++ backend/Data/DataContext.cs
@@ -11,9 +11,9 @@ namespace Data
     public class DataContext : DbContext
     {
         public DbSet<Monster> Monsters { get; set; }
-        // public DbSet<Location> Location { get; set; }
+        public DbSet<Location> Location { get; set; }
         public DbSet<SavableOpenedMonster> Creation { get; set; }
-        // public DbSet<OpenedMonster> Combat { get; set; }
+        public DbSet<OpenedMonster> Combat { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
