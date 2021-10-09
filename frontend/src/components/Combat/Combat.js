ad4b1677be30ed77335629dedd996f48f10cf18d
Lauras Dilys <lauras.dilys@gmail.com>
2021-10-10 02:55:12 +0300

    added redirection to creation

--- frontend/src/components/Combat/Combat.js
+++ frontend/src/components/Combat/Combat.js
@@ -2,15 +2,22 @@ import { useSelector } from "react-redux";
 import { monstersState } from "../../state/selectors";
 import CombatantList from "./CombatantList";
 import './Combat.css'
+import { Fab } from "@mui/material";
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import { useHistory  } from "react-router-dom";
 
 const Combat = () => {
   const { monsters } = useSelector(monstersState);
+  let history = useHistory();
 
   return (
     <div className="combat-selection-row">
       <div className="combat-selection-col">
         {monsters.length > 0 ? <CombatantList /> :
-        <p>Create some monsters</p>}
+        <Fab variant="extended" onClick={() => history.push('/creation')}>
+          <NavigateBeforeIcon sx={{ mr: 1 }}/>
+          Create some monsters first
+        </Fab>}
       </div>
     </div>
   );
